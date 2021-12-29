54d0acea9f092699f220062f4fa16ce7c646f6db
Phuthuyxam <48133351+Phuthuyxam@users.noreply.github.com>
2021-12-30 00:05:23 +0700

    iii

--- build_image.sh
+++ build_image.sh
@@ -4,5 +4,5 @@ docker rm --force ecs_study
 
 docker build -t ecs_study_image:latest -f docker/Dockerfile .
 
-docker run --privileged -ti -d -p 9000:80 --name ecs_study ecs_study_image:latest   
+docker run --rm --privileged=true -ti -d -p 9000:80 --name ecs_study ecs_study_image:latest /sbin/init
 
